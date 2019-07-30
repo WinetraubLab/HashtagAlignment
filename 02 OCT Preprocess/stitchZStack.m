fa1a049338039823be65d5a5846c5492796ffea5
MyYo <MyYo@users.noreply.github.com>
2019-07-30 13:52:40 -0700

    Bug fixes

--- 02 OCT Preprocess/stitchZStack.m
+++ 02 OCT Preprocess/stitchZStack.m
@@ -35,15 +35,19 @@ yToSave = dim.y.index(...
     round(linspace(1,length(dim.y.index),saveYs)) ...
     );
 
+pixSizeZ = diff(dim.z.values([1 2])); %um
+
 %% Preform stitching
 disp('Stitching ... '); tt=tic();
 yIndexes=dim.y.index;
-imOut = zeros([size(scan1) length(yIndexes)]); %[z,x,y]
-for yI=yIndexes %Loop over y frames
+imOut = zeros([length(dim.z.values) length(dim.x.values) length(yIndexes)]); %[z,x,y] 
+thresholds = zeros(1,length(yIndexes));
+imOutSize = size(imOut);
+parfor yI=1:length(yIndexes) %Loop over y frames
     fprintf('%s Processing yI=%d of %d.\n',datestr(datetime),yI,length(yIndexes));
     
     %Loop over depths
-    stack = zeros([size(imOut), length(zToScan)])*NaN;
+    stack = zeros([imOutSize(1:2), length(zToScan)])*NaN; %z,x,zStach
     for zzI=1:length(zToScan)
         
         %Load Frame
@@ -59,23 +63,38 @@ for yI=yIndexes %Loop over y frames
         %Filter
         zI = 1:length(scan1); zI = zI(:);
         factor = repmat(exp(-(zI-focusPositionInImageZpix).^2/(2*focusSigma)^2), [1 size(scan1,2)]);
+        %factor(factor<exp(-1/2*3^2)) = 0; %Values too low / out of focus
           
         %Add to stack
-        stack(:,:,zzI) = imtranslate((scan1.*factor),[0,zToScan(zzI)/n],'FillValues',NaN); 
+        stack(:,:,zzI) = imtranslate((scan1.*factor),[0,zToScan(zzI)/pixSizeZ],'FillValues',NaN); 
             %In translation, compensate for water/tissue numerical
             %apperature
             
         %Save for overview Purpose
-        if (sum(yI == yToSave)>0)
-            yOCT2Tif(log(scan1),sprintf('%s/BScan_Y%03d.tif',fp(zzI),yI));
+        if (sum(yIndexes(yI) == yToSave)>0)
+            yOCT2Tif(log(scan1),sprintf('%s/BScan_Y%03d.tif',fpTxt,yIndexes(yI)));
         end
     end
     
     imOut(:,:,yI) = nanmean(stack,3);
+    
+    %Since we are dealing with a log scale, its important to trim the image
+    %Let us trim the image using the signal at the gel (top of the image)
+    tmp = nanmedian(squeeze(stack(:,:,1)),2);
+    thresholds(yI) = max(tmp(:))/size(stack,3)/2; %Devided by the amount of averages
 end
 fprintf('Done stitching, toatl time: %.0f[min]\n',toc(tt)/60);
 
+%% Average
+th = mean(thresholds);
+imOut1 = imOut;
+imOut1(imOut1<th) = th;
+
 %% Output Tiff
 disp('Saving to Tiff ...');
-yOCT2Tif(log(imOut),[OCTVolumesFolder '/VolumeScanAbs.tif']);
+yOCT2Tif(log(imOut1),[OCTVolumesFolder '/VolumeScanAbs.tif']);
 disp('Done');
+
+figure(1);
+imagesc(squeeze(log(imOut1(:,:,round(size(imOut1,3)/2)))));
+colormap bone;
