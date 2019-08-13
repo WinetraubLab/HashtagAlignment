3069cc47b18ff3c530e373ab6e6bdf82356aec87
MyYo <MyYo@users.noreply.github.com>
2019-08-13 14:18:38 -0700

    Make tall.write work from Cloud

--- 02 OCT Preprocess/tallWriter.m
+++ 02 OCT Preprocess/tallWriter.m
@@ -1,5 +1,10 @@
 function tallWriter (info, data)
-filename = [info.RequiredLocation '\' strrep(info.RequiredFilePattern,'*','')];%Remove required pattern, its easier that way
-%filename = info.SuggestedFilename;
-yOCT2Mat(data{:}, filename);
+if (info.PartitionIndex ~= 1) || (info.BlockIndexInPartition ~= 1)
+    error('info.PartitionIndex = %d, info.BlockIndexInPartition = %d - both should be 1',...
+        info.PartitionIndex,info.BlockIndexInPartition);
+end
+ff = strrep(info.RequiredFilePattern,'*','1');%Remove required pattern, its easier that way
+filename1 = sprintf('%s/%s',info.RequiredLocation, ff);
+%filename1 = info.SuggestedFilename;
+yOCT2Mat(data{:}, filename1);
 end
