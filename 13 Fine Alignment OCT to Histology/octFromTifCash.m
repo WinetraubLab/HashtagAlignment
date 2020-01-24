e493757b108f8c941523260e6d7a9a2b112549cb
MyYo <MyYo@users.noreply.github.com>
2020-01-24 14:18:42 -0800

    Add manual fine alignment GUI

---
+++ 13 Fine Alignment OCT to Histology/octFromTifCash.m
@@ -0,0 +1,24 @@
+function dat = octFromTifCash(filePath, yI)
+%This fuction returns an OCT plane but reads it slowly from cash
+
+persistent gFilePath;
+persistent gCashData;
+
+%% Make sure cash is up to date
+if isempty(gFilePath) || ~strcmp(filePath,gFilePath)
+    % Time to clear cash
+    gCashData = {};
+    gFilePath = filePath;
+end
+
+%% Is this index in cash?
+if (yI <= length(gCashData) && ~isempty(gCashData{yI}))
+    % Cashed, do nothing
+else
+    %Index not cashed, load it
+    gCashData{yI} = yOCTFromTif(filePath,'yI',yI);
+end
+
+dat = gCashData{yI}; % This data is cashed
+
+    
