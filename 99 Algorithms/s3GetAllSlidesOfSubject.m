de73854f6141c605462987858d7be65cdb691d44
MyYo <MyYo@users.noreply.github.com>
2020-02-20 13:26:50 -0800

    Slide name should not end with '/'

--- 99 Algorithms/s3GetAllSlidesOfSubject.m
+++ 99 Algorithms/s3GetAllSlidesOfSubject.m
@@ -6,3 +6,10 @@ sectionRootFolder = awsModifyPathForCompetability([subjectPath '/Slides/']);
 isFolder = cellfun(@(x)(contains(x,'/')),sectionNames);
 sectionNames = sectionNames(isFolder);
 sectionPaths = sectionPaths(isFolder);
+
+% Remove last '/' from section Names
+for i=1:length(sectionNames)
+    if sectionNames{i}(end) == '/' || sectionNames{i}(end) == '\'
+        sectionNames{i}(end) = [];
+    end
+end
