cd4a93df1b6a94b1e4880d69ca22715acf9b6bb2
MyYo <MyYo@users.noreply.github.com>
2020-03-27 18:14:10 -0700

    More nobes for improvements

--- 21 ML Preprocess/scriptGeneratePatches.m
+++ 21 ML Preprocess/scriptGeneratePatches.m
@@ -6,14 +6,15 @@
 libraryNames = {'LE','LF'};
 
 % Output patches defenitions.
-patchSize_pix = 256; % Patch size (pixels)
+patchSizeX_pix = 256; % Patch size (pixels)
+patchSizeY_pix = 256;
 
 % Magnification - Approximate Pixel Size
 %           20x - 0.7 micron
 %           10x - 1.1 micron
 %            4x - 2.7 micron
 % Source: https://www.amscope.com/camera-resolution
-patchPixelSize = 2; % microns.
+patchPixelSize = 2; % microns, original image size: 1um.
 
 % Where to write patches to:
 outputFolder = 'Patches/';
@@ -23,9 +24,18 @@ patchOverlap = 32; % Pixels.
 patchDataMinimum = 0.5; % Reject patch if less than x% of its area is usable.
 includeflip = true; % Mirror flip patch as well.
 
-% Jenkins override of inputs
+%% Jenkins override of inputs
 if exist('patchFolder_','var')
     outputFolder = patchFolder_;
+end
+if exist('patchPixelSize_','var')
+    patchPixelSize = patchPixelSize_;
+end
+if exist('patchSizeX_pix_','var')
+    patchSizeX_pix = patchSizeX_pix_;
+end
+if exist('patchSizeY_pix_','var')
+    patchSizeY_pix = patchSizeY_pix_;
 end
 
 %% Clear output folder
@@ -87,33 +97,35 @@ for iSlide=1:length(isUsable)
     img_oct = repmat(scale0To255(img_oct),[1 1 3]);
      
     % Pad with 0s if we cropped too much
-    img_he = imPadToMeetPatchSize(img_he, patchSize_pix, 0);
-    img_oct = imPadToMeetPatchSize(img_oct, patchSize_pix, 0);
-    img_mask = imPadToMeetPatchSize(img_mask, patchSize_pix, 10); 
+    img_he = imPadToMeetPatchSize(img_he, patchSizeX_pix, patchSizeY_pix, 0);
+    img_oct = imPadToMeetPatchSize(img_oct, patchSizeX_pix, patchSizeY_pix, 0);
+    img_mask = imPadToMeetPatchSize(img_mask, patchSizeX_pix, patchSizeY_pix, 10); 
     
     % define h -height and w-width of images after downsize
     h=size(img_he,1); w=size(img_he,2);
     
-    % Recolor to identify empty regions.
-    img_he = imReColor(img_he,[0 0 0],[0 255 0]);
-    img_oct = imReColor(img_oct,[0 0 0],[0 255 0]);
+    % Recolor to identify empty regions. (no value backgroud)
+    noValueBackground = [0,0,0]; % Black background.
+    %noValueBackground = [0,255,0]; % Green background.
+    img_he = imReColor(img_he,[0 0 0],noValueBackground); %RGB
+    img_oct = imReColor(img_oct,[0 0 0],noValueBackground); %RGB
     
     %% Generate patches 
-    if h<patchSize_pix || w<patchSize_pix
+    if h<patchSizeY_pix || w<patchSizeX_pix
         error('Should never happen');
         %this image is too small to generate patches.
     end 
 
     % loop through patches
     cropcount = 1;
-    for y=1:patchOverlap:(h-patchSize_pix)
-        for x =1:patchOverlap:(w-patchSize_pix)
-            roiy = y:y+(patchSize_pix-1);
-            roix = x:x+(patchSize_pix-1);
+    for y=1:patchOverlap:(h-patchSizeY_pix)
+        for x =1:patchOverlap:(w-patchSizeX_pix)
+            roiy = y:y+(patchSizeY_pix-1);
+            roix = x:x+(patchSizeX_pix-1);
 
             % Reject image if # of non-zeros pixels greater than threshold
             goodPixels = sum(sum(img_mask(roiy,roix) < 0.2));
-            totalPixels = patchSize_pix^2;
+            totalPixels = patchSizeX_pix*patchSizeY_pix;
             if goodPixels/totalPixels<patchDataMinimum
                 warning('%s-%s have less than %.0f%% of usable pixel, therefore is beeing skipped.', ...
                 subjectName,sectionName,patchDataMinimum*100);
