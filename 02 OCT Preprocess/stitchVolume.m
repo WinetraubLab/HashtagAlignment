0c5ca09bcd3f72e3abd8f1d2bb4a37bc8fabc5f8
Ziv Lautman <44987598+lautman@users.noreply.github.com>
2020-07-19 17:38:05 +0300

    Mac compatibility

--- 02 OCT Preprocess/stitchVolume.m
+++ 02 OCT Preprocess/stitchVolume.m
@@ -1,8 +1,9 @@
 %This script stitches images aquired at different z depths 2gezer
-
+%Mac Only:
+setenv('PATH', [getenv('PATH') ':/usr/local/bin']);
 %OCT Data
-OCTVolumesFolder = [s3SubjectPath('01') 'OCTVolumes/'];
-dispersionQuadraticTerm = []; %Use default that is specified in ini probe file
+OCTVolumesFolder = 's3://delazerdamatlab/Users/BrainProject/6-26-Ganymede20x/';
+dispersionQuadraticTerm = [8e07]; %Use default that is specified in ini probe file
 
 focusSigma = 20; %When stitching along Z axis (multiple focus points), what is the size of each focus in z [pixel]
 outputFileFormat = 'tif'; %Can be 'tif' or 'mat' for debug
@@ -17,9 +18,9 @@ end
 
 %% Directories 
 OCTVolumesFolder = awsModifyPathForCompetability([OCTVolumesFolder '\']);
-SubjectFolder = awsModifyPathForCompetability([OCTVolumesFolder '..\']);
+SubjectFolder = awsModifyPathForCompetability([OCTVolumesFolder '\']);
 
-logFolder = awsModifyPathForCompetability([SubjectFolder '\Log\02 OCT Preprocess\VolumeDebug\']);
+logFolder = awsModifyPathForCompetability([SubjectFolder '\Log\VolumeDebug\']);
 outputFolder = awsModifyPathForCompetability([OCTVolumesFolder '/VolumeScanAbs/']);
 
 %% Process scan
@@ -32,12 +33,14 @@ end
 if isRunOnJenkins
     setupParpolOCTPreprocess();
 end
+tic;
 yOCTProcessTiledScan(...
-        [OCTVolumesFolder 'Volume\'], ... Input
-        {outputFolder [outputFolder(1:(end-1)) '_All.tif']},...
+        [OCTVolumesFolder 'Volume/'], ... Input
+        {outputFolder [outputFolder(1:(end-1)) '_All2.tif']},...
         'focusPositionInImageZpix',json.focusPositionInImageZpix,... No Z scan filtering
 		'focusSigma',focusSigma,...
         'dispersionQuadraticTerm',dispersionQuadraticTerm,...
 		'yPlanesOutputFolder',[logFolder 'Debug\'],...
         'howManyYPlanes',3,... Save some raw data ys if there are multiple depths
         'v',true);
+fprintf('yOCTProcessTiledScan function run time: %d min and %.1f seconds\n', fix(toc/60),rem(toc,60));
