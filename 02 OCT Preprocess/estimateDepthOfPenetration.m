468785dd1b33ec8a3b78f7241ac66d12149ca2e1
MyYo <MyYo@users.noreply.github.com>
2020-07-24 00:07:22 -0700

    Main issues 1) Add a switch to determine if uploading or not 2) Generate a top view 2D image of the interface 3) Smooth interface for better view

--- 02 OCT Preprocess/estimateDepthOfPenetration.m
+++ 02 OCT Preprocess/estimateDepthOfPenetration.m
@@ -1,4 +1,5 @@
-% This script estimates OCT light depth of penetration.
+% This script estimates OCT light depth of penetration, tissue intefrace
+% position and max depth for evry point in the volume.
 % - Script for evaluating the depth of penetration in a given stitched volume. 
 % - The script will generate figures of different slices for visualization and error detection.
 
@@ -8,13 +9,19 @@
 OCTVolumesFolder = [s3SubjectPath('06','LC') 'OCTVolumes/'];
 %OCTVolumesFolder = [s3SubjectPath('01') 'OCTVolumes/'];
 
+isUploadToAWS = false; % Set to true if you would like to upload to aws
+
 %% Jenkins
 if (exist('OCTVolumesFolder_','var'))
     OCTVolumesFolder = OCTVolumesFolder_;
+    isUploadToAWS = true;
 end
 
 %% Set folders
 logPath = awsModifyPathForCompetability([OCTVolumesFolder '../Log/02 OCT Preprocess/DepthOfPenetration/']);
+if isUploadToAWS && awsExist(logPath)
+    awsRmDir(logPath); %Clear before upload
+end
 
 % Load scan config json for additional information
 scanConfigPath = [OCTVolumesFolder '/ScanConfig.json'];
@@ -23,11 +30,13 @@ scanConfigJson = awsReadJSON(scanConfigPath);
 %% Load Volume information
 [~,meta] = yOCTFromTif([OCTVolumesFolder '/VolumeScanAbs/'],'isLoadMetadataOnly',true);  
 zPixelSize_um = diff(meta.z.values(1:2))*1e3; % um/pix
+xPixelSize_um = diff(meta.x.values(1:2))*1e3; % um/pix
 
 % Select bScans to load, around the center of the scan as this is the
 % relevant area.
-bScanIndexs = meta.y.index(round(linspace(0,length(meta.y.index)-1,21)+0.5));
+bScanIndexs = meta.y.index(round(linspace(0,length(meta.y.index)-1,41)+0.5));
 bScanIndexs([1 end]) = [];
+bScanIndexs = [1;bScanIndexs;length(meta.y.index)];
 
 % Change z coordinate system (if required) such that z=0 is the focus positoin of OCT image when zDepths=0 scan was taken.
 if ~isfield(meta.z,'origin') || contains(meta.z.origin,'top of OCT image')
@@ -45,6 +54,8 @@ depthOfPenetrations = [];
 tissueTopPositions = [];
 surfaceIntensitys = [];
 noiseIntensitys = [];
+tissueGelInterfaceZ_um = NaN*zeros(length(meta.y.values),length(meta.x.values));
+maxLightPenetrationZ_um = tissueGelInterfaceZ_um;
 for BScanI=1:length(bScanIndexs)
     
     % Load a single B-Scan, saves time
@@ -86,6 +97,9 @@ for BScanI=1:length(bScanIndexs)
     surfaceZPosition_um = interp1(xI(~nn),meta.z.values(round(surfaceZPosition_px(~nn)))*1e3,xI);
     surfaceZPosition_um = surfaceZPosition_um(:)';
     
+    % Smooth, interface needs to be smooth to 10um level
+    surfaceZPosition_um = mySmooth(surfaceZPosition_um, 10/xPixelSize_um);
+    
     %% Step #2 - find deepest visible area (using SNR)
     
     % Make an average OCT signal.
@@ -113,31 +127,62 @@ for BScanI=1:length(bScanIndexs)
     maxDepthPosition_px(val ~= 1) = NaN; % x positions where no bound was found are nans
     maxDepthPosition_um(val ~= 1) = NaN; % x positions where no bound was found are nans
     
+    % Smooth, interface needs to be smooth to 10um level
+    maxDepthPosition_um = imgaussfilt(maxDepthPosition_um, 10/xPixelSize_um);
+    
     %% Depth of penetration
     depthOfPenetration = maxDepthPosition_um(:)' - surfaceZPosition_um(:)';
     depthOfPenetration = depthOfPenetration(:)';
     
     %% Plot output
-    figure(12);
-    imagesc(meta.x.values*1e3,meta.z.values*1e3,imOCT); colormap gray
-    title(sprintf('#%d OCT B-Scan, yI=%d\nDepth of Penetration: %.0f[\\mum]',...
-        BScanI, bScanIndexs(BScanI), nanmedian(depthOfPenetration)));
-    hold on;
-    plot(meta.x.values*1e3,surfaceZPosition_um);
-    plot(meta.x.values*1e3,maxDepthPosition_um);
-    hold off;
-    xlabel('x[\mum]');
-    ylabel('z[\mum]');
-    legend('Tissue Surface','Max Depth');
-    grid on;
-    awsSaveMatlabFigure(gcf,sprintf('%s/y%04d.png',logPath,bScanIndexs(BScanI)));
+    if mod(bScanIndexs(BScanI),100)==0
+        figure(12);
+        imagesc(meta.x.values*1e3,meta.z.values*1e3,imOCT); colormap gray
+        title(sprintf('#%d OCT B-Scan, yI=%d\nDepth of Penetration: %.0f[\\mum]',...
+            BScanI, bScanIndexs(BScanI), nanmedian(depthOfPenetration)));
+        hold on;
+        plot(meta.x.values*1e3,surfaceZPosition_um);
+        plot(meta.x.values*1e3,maxDepthPosition_um);
+        hold off;
+        xlabel('x[\mum]');
+        ylabel('z[\mum]');
+        legend('Tissue Surface','Max Depth');
+        grid on;
+        
+        if isUploadToAWS
+            awsSaveMatlabFigure(gcf,sprintf('%s/y%04d.png',logPath,bScanIndexs(BScanI)));
+        end
+    end
     
     %% Capture data
     depthOfPenetrations = [depthOfPenetrations depthOfPenetration(~isnan(depthOfPenetration))];
     tissueTopPositions = [tissueTopPositions surfaceZPosition_um(~isnan(surfaceZPosition_um))];
     surfaceIntensitys = [surfaceIntensitys surfaceIntensity];
     noiseIntensitys = [noiseIntensitys minIntensity];
+    
+    tissueGelInterfaceZ_um(bScanIndexs(BScanI),:) = surfaceZPosition_um;
+    maxLightPenetrationZ_um(bScanIndexs(BScanI),:) = maxDepthPosition_um;
 end
+
+%% Interpolate tissue interface positions
+[yyi,xxi] = meshgrid(1:size(tissueGelInterfaceZ_um,1),1:size(tissueGelInterfaceZ_um,2));
+tissueGelInterfaceZ_um = interp2(...
+    yyi(bScanIndexs,:),xxi(bScanIndexs,:),tissueGelInterfaceZ_um(bScanIndexs,:),yyi,xxi,'spline');
+maxLightPenetrationZ_um = interp2(...
+    yyi(bScanIndexs,:),xxi(bScanIndexs,:),maxLightPenetrationZ_um(bScanIndexs,:),yyi,xxi,'spline');
+
+% Save
+if isUploadToAWS
+    metaData = meta;
+    metaData.z = [];
+    metaData.z.units = 'um';
+    metaData.z.meaning = 'for each pixel value is depth in microns. z=0 is where the focal position was at user selected scan start depth';
+    scanConfigJson.volumeStatistics.tissueGelInterfaceImagePath = 'tissueGelInterfaceZ_um.tif';
+    yOCT2Tif(tissueGelInterfaceZ_um,[OCTVolumesFolder '/TissueGelInterfaceZ_um.tif'],'metadata',metaData);
+    scanConfigJson.volumeStatistics.depthOfPenetrationImagePath = 'maxLightPenetrationZ_um.tif';
+    yOCT2Tif(maxLightPenetrationZ_um,[OCTVolumesFolder '/MaxLightPenetrationZ_um.tif'],'metadata',metaData);
+end
+
 
 %% Plot overall statistics
 figure(12);
@@ -146,14 +191,18 @@ grid on;
 title (sprintf('Depth of Penetration at Different X-Y Positions\n Median: %.0f[\\mum]',...
     median(depthOfPenetrations)));
 xlabel('Depth of Penetration [\mum]');
-awsSaveMatlabFigure(gcf,[logPath 'DepthOfPenetrations.png']);
+if isUploadToAWS
+    awsSaveMatlabFigure(gcf,[logPath 'DepthOfPenetrations.png']);
+end
 
 figure(12);
 histogram(tissueTopPositions,20);
 grid on;
 title ('Tissue-Gel Interface Z at Different X-Y Positions');
 xlabel(sprintf('Top of Tissue Z[\\mum]\nZ=0 is user selected focus position. Z>0 means deeper.'));
-awsSaveMatlabFigure(gcf,[logPath 'TissueInterfacePositions.png']);
+if isUploadToAWS
+    awsSaveMatlabFigure(gcf,[logPath 'TissueInterfacePositions.png']);
+end
 
 %% Save Statistics
 scanConfigJson.volumeStatistics.depthOfPenetration_um = median(depthOfPenetrations);
@@ -169,4 +218,11 @@ scanConfigJson.volumeStatistics.notes = [ ...
     'noiseFloor_db - intensity of the signal we consider to be the bare minimum to have detection. Units: dn.' newline ...
     ];
 
-awsWriteJSON(scanConfigJson,scanConfigPath);
+if isUploadToAWS
+    awsWriteJSON(scanConfigJson,scanConfigPath);
+end
+
+function y = mySmooth(x, sigma)
+y = imgaussfilt(x,sigma);
+y(isnan(y)) = x(isnan(y));
+end
