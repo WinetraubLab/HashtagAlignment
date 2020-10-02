ba85f676abe7d4f9f5a3bcdbe86eaf09c9a0af50
MyYo <MyYo@users.noreply.github.com>
2020-10-02 06:56:26 -0700

    Generate 10x first

--- 21 Generate Dataset/scriptGenerateDataset_multipleRes.m
+++ 21 Generate Dataset/scriptGenerateDataset_multipleRes.m
@@ -1,9 +1,8 @@
 % This script generate datasets in multiple resolutions and scales
 
+call_scriptGenerateDataset('10x');
 call_scriptGenerateDataset('4x');
 call_scriptGenerateDataset('2x');
-call_scriptGenerateDataset('10x');
-
 fprintf('%s Done Done!\n',datestr(datetime));
 
 function call_scriptGenerateDataset(magnificationName_)
