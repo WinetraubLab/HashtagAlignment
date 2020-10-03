8f34f559e1c8d4455a02132f7ab9787e62cb1624
MyYo <MyYo@users.noreply.github.com>
2020-10-03 16:39:42 -0700

    This script downloads latest dataset locally

---
+++ 23 Visualize Results/scriptViewDataset.m
@@ -0,0 +1,10 @@
+% This script downloads dataset to local folder to be viewed by user
+
+%% Inputs
+imageResolution = '10x';
+datasetTag = '';%'2020-08-15'; % What date this data set was created, leave empty for latest
+
+%% Gather dataset
+datasetPath = getPathToLatestDataset(imageResolution,datasetTag);
+awsMkDir('tmp/',true);
+awsCopyFileFolder(datasetPath,'tmp/');
