c669a71cc1fa702190cee22523919d11eb66fa07
edyuan <edy.yuan@gmail.com>
2021-04-01 17:59:01 -0700

    Modified load_dataset function to take in two separate data folders for OCT and histology. Made other minor changes in the comments as well

--- 32 NN Model/input_pipeline.py
+++ 32 NN Model/input_pipeline.py
@@ -6,67 +6,90 @@ import random
 Constructs a TensorFlow dataset object
 
 	Parameters:
-		data_folder (string) : A file path of the aligned OCT and Histology images
-		isTrain     (boolean): Indicates whether the data_folder is pointing to train data or test data
+		OCT_data_folder  (string) 			- A file path pointing to the folder of OCT images
+		hist_data_folder (string, optional) - A file path pointing to the folder of histology images. This value can 
+											  be blank, in which case the returned TensorFlow dataset object cannot 
+											  be used for training (isTrain = False).
+											  ** NOTE: We expect corresponding OCT and histology images to have the 
+											  		   same file name across both data folders **
+
+		isTrain     	 (boolean)			- Indicates whether the OCT_data_folder and hist_data_folder are pointing 
+											  to train data or test data. 
+											  ** NOTE: When generating the train dataset, we introduce randomization 
+											  		   in the form of reshuffling and additional pre-processing to make
+											  		   the algorithm more robust. For more information, see docstring 
+											  		   for the _preprocess_image function below. **
 
 	Returns:
-		dataset (tf.data.Dataset) : A TensorFlow dataset object containing images from data_folder
+		dataset 		 (tf.data.Dataset)  - A TensorFlow dataset object containing images from data_folder
 '''
-def load_dataset(data_folder, isTrain=True):
+def load_dataset(OCT_data_folder, hist_data_folder='', isTrain=True):
 
 	BUFFER_SIZE = 400
 	BATCH_SIZE = 1
 
-	# Creates a dataset of all files matching the glob pattern jpg
-	dataset = tf.data.Dataset.list_files(data_folder+'*.jpg') 
+	# Create OCT and histology datasets of all files matching the glob pattern jpg
+	OCT_dataset = tf.data.Dataset.list_files(OCT_data_folder+'*.jpg', shuffle=False) 
+	if hist_data_folder != '':
+		hist_dataset = tf.data.Dataset.list_files(hist_data_folder+'*.jpg', shuffle=False) 
+		isTrain = False
 
 	if isTrain:
 
-		# Applies the preprocess_image function to each element of this dataset and returns
-		# a new dataset containing the transformed elements, in the same order as they appeared in the input
-		# NOTE: num_parallel was only specified for train not test (this may need to be changed)
-		dataset = dataset.map(lambda x: preprocess_image(x, isTrain), num_parallel_calls=tf.data.AUTOTUNE) 
+		# Apply the _preprocess_image function to each element of the OCT and histology datasets and return
+		# new datasets containing the transformed elements, in the same order as they appeared before pre-processing
+		OCT_dataset = OCT_dataset.map(lambda x: preprocess_image(x, isTrain), num_parallel_calls=tf.data.AUTOTUNE) 
+		hist_dataset = hist_dataset.map(lambda x: preprocess_image(x, isTrain), num_parallel_calls=tf.data.AUTOTUNE) 
 
-		# Randomly shuffles the elements of this dataset
-		# This dataset fills a buffer with BUFFER_SIZE elements, then randomly samples elements from this buffer,
+		# Pair the corresponding OCT and histology images together
+		dataset = tf.data.Dataset.zip((OCT_dataset, hist_dataset))
+
+		# Randomly shuffle the elements of the dataset
+		# The dataset fills a buffer with BUFFER_SIZE elements, then randomly samples elements from this buffer,
 		# replacing the selected elements with new elements. For perfect shuffling, a buffer size >= the full size 
 		# of the dataset is needed
 		dataset = dataset.shuffle(BUFFER_SIZE)
 
 	else:
 
-		# Applies the preprocess_image function to each element of this dataset and returns
-		# a new dataset containing the transformed elements, in the same order as they appeared in the input
-		dataset = dataset.map(lambda x: preprocess_image(x, isTrain)) 
+		# Apply the _preprocess_image function to each element of the OCT dataset and return
+		# a new dataset containing the transformed elements, in the same order as they appeared before pre-processing
+		OCT_dataset = OCT_dataset.map(lambda x: _preprocess_image(x, isTrain)) 
 
-	# Combines consecutive elements of this dataset into batches 
+		# If histology images were provided, apply the _preprocess_image function to each element of the histology
+		# dataset and return a new dataset containing the transformed elements, in the same order as they appeared 
+		# before pre-processing. Pair the corresponding OCT and histology images together
+		if hist_data_folder != '':
+			hist_dataset = hist_dataset.map(lambda x: _preprocess_image(x, isTrain)) 
+			dataset = tf.data.Dataset.zip((OCT_dataset, hist_dataset))
+		else:
+			# If no histology images are provided in the test set, duplicate the OCT images in the dataset for
+			# tensor format consistency 
+			dataset = tf.data.Dataset.zip((OCT_dataset, OCT_dataset)) 
+
+	# Combine consecutive elements of this dataset into batches 
 	# The components of the resulting element will have an additional outer dimension which will be BATCH_SIZE
-	dataset = dataset.batch(BATCH_SIZE)
+	dataset = dataset.batch(BATCH_SIZE) 
 
-	return dataset
+	return dataset 
 
 '''
-Applies preprocessing steps to OCT+Histology image pair. The first step is to separate the OCT+Histology pair
-into two separate images. If the OCT and histology images are from the train set, they are randomly translated, 
-resized to 286 x 286, randomly cropped to be 256 x 256, and then some images are randomly chosen to be mirrored. 
-If the OCT anf histology images are from the test set, they are only resized to 256 x 256. Images from the train 
-and test set are both normalized to have values between -1 and 1. 
+Applies preprocessing steps to the input image. 
+
+If the image is from the train set, it is randomly translated, resized to 286 x 286, randomly cropped to be 256 x 256,
+and then the image may be randomly chosen to be mirrored. If the image is from the test set, it is only resized to 
+256 x 256. Images from the train and test set are both normalized to have values between -1 and 1. 
 
 	Parameters: 
-		image_file (string)  : A file path to the OCT+Histology image pair 
+		image_file (string)  : A file path to the image (OCT or histology)
 		isTrain    (boolean) : Indicates whether the image_file is part of the train data or test data
 
 	Returns:
-		input_image (Tensor) : The preprocessed OCT image
-		real_image  (Tensor) : The preprocessed histology image
+		preprocessed_image (Tensor) : The preprocessed OCT image
 '''
-def preprocess_image(image_file, isTrain):
+def _preprocess_image(image_file, isTrain):
 
-	# Read in the OCT+Histology image pair and decode the JPEG-encoded image to a uint8 tensor
-
-	# Extract the OCT image and cast it as a set of floats
-
-	# Extract the histology image and cast it as a set of floats
+	# Read in the image, decode the JPEG-encoded image to uint8 tensor, and cast it as a set of floats
 
     if isTrain:
         # translate images by a random amount to increase robustness (only if we are in training mode)
@@ -84,5 +107,5 @@ def preprocess_image(image_file, isTrain):
 
 	# normalize image values to be in range [-1, 1]
 
-	return input_image, real_image
+	return preprocessed_image
 
