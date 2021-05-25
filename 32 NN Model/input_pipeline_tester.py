ec5d89691124905c91078dd5c38c8bf5fab8eef1
Jessica <jt658@stanford.edu>
2021-05-25 15:07:33 -0700

    I changed to assert statements to use assertAll in order to properly compare all elements of the Tensors. I also added two new test cases that verify the exact dimensions of the OCT and histology images

--- 32 NN Model/input_pipeline_tester.py
+++ 32 NN Model/input_pipeline_tester.py
@@ -15,20 +15,32 @@ class InputPipelineTest(tf.test.TestCase):
 
         self.rtj_OCT_image1, self.rtj_OCT_image2 = ip.random_translate_jitter(self.OCT_image, self.OCT_image)
 
-    # Verify that the spatial dimensions of the OCT and histology image match up
-    def test_random_translate_jitter_dimensions(self):
-        self.assertEqual(tf.shape(self.rtj_OCT_image)[0:1], tf.shape(self.rtj_hist_image)[0:1],
-                         msg="OCT and histology images must both have the same width and height.")
+    # Verify that the dimensions of the OCT and histology image match up
+    def test_random_translate_jitter_matching_dimensions(self):
+        self.assertAllEqual(tf.shape(self.rtj_OCT_image), tf.shape(self.rtj_hist_image),
+                         msg="OCT and histology images must both have the same width, height, and number of channels.")
+
+    # Verify that the dimensions of the OCT image is 256x256x3
+    def test_random_translate_jitter_OCT_correct_dimensions(self):
+        self.assertAllEqual(tf.shape(self.rtj_OCT_image), tf.shape(tf.ones([256, 256, 3])),
+                            msg="OCT images must both have shape 256x256x3.")
+
+    # Verify that the dimensions of the histology image is 256x256x3
+    def test_random_translate_jitter_hist_correct_dimensions(self):
+        self.assertAllEqual(tf.shape(self.rtj_OCT_image), tf.shape(tf.ones([256, 256, 3])),
+                            msg="Histology images must both have shape 256x256x3.")
 
     # Verify that both OCT images undergo the same random transformation
     def test_random_translate_jitter_consistency(self):
-        self.assertEqual(self.rtj_OCT_image1, self.rtj_OCT_image2,
+        self.assertAllEqual(self.rtj_OCT_image1, self.rtj_OCT_image2,
                          msg="Both input images must undergo the same random transformation")
 
     # Verify that the transformation (translation+jitter+flip) is not the same for every OCT+hist image pair
     def test_random_translate_jitter_deterministic(self):
-        self.assertNotEqual(self.rtj_OCT_image, self.rtj_OCT_image1,
+        self.assertNotAllEqual(self.rtj_OCT_image, self.rtj_OCT_image1,
                             msg="Both input images must undergo the same random transformation")
+
+    # Verify that the dimensions of the OCT and histology image match up for resize function
 
 
 if __name__ == '__main__':
