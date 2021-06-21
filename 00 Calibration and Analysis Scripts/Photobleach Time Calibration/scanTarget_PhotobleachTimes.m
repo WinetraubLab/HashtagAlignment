0c4c8d4798b03d3c86b786ae71fc557d4bda14e0
MyYo <MyYo@users.noreply.github.com>
2021-06-21 15:31:56 -0700

    Update photobleaching

--- 00 Calibration and Analysis Scripts/Photobleach Time Calibration/scanTarget_PhotobleachTimes.m
+++ 00 Calibration and Analysis Scripts/Photobleach Time Calibration/scanTarget_PhotobleachTimes.m
@@ -3,14 +3,21 @@
 
 %% Inputs
 
+% Use this as initial set
 exposures = [1 2 4 5 15 30]; %Units are sec per 1mm of line
 nPasses =   [2 2 2 2 2  2 ]; % Number of passes should be as low as possible but still allow OCT scanner not to crash
+
+% Fine tuning for 40x
+exposures = [1 2 5 10 15]; %Units are sec per 1mm of line
+nPasses =   [2 2 2 2   2]; % Number of passes should be as low as possible but still allow OCT scanner not to crash
+
 
 % Photobleach pattern configuration
 octProbePath = getProbeIniPath('40x');
 
 lineLength = 1; %mm
-x = linspace(-lineLength/2,lineLength/2,length(exposures)); % X Positions of the lines
+x = linspace(-lineLength/2,lineLength/2,length(exposures)+2); % X Positions of the lines
+x([1 end]) = [];
 
 %% Setup 
 ThorlabsImagerNETLoadLib(); %Init library
