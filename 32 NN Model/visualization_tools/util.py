f1663b64c1fdccfa1e1e075ec8257ea29adb8977
jt658 <jt658@stanford.edu>
2021-06-23 13:46:59 -0700

    Changes save_image function to resize images to original dimensions

--- 32 NN Model/visualization_tools/util.py
+++ 32 NN Model/visualization_tools/util.py
@@ -25,23 +25,21 @@ def tensor2im(input_image, imtype=np.uint8):
         image_numpy = input_image
     return image_numpy.astype(imtype)
 
-
-def save_image(image_numpy, image_path, aspect_ratio=1.0):
-    """Save a numpy image to the disk
+'''Save a numpy image to the disk
 
     Parameters:
         image_numpy (numpy array) -- input numpy array
         image_path (str)          -- the path of the image
-    """
+        original_image_dims (tuple) -- the dimensions of the original image before pre-processing
+'''
 
+def save_image(image_numpy, image_path, original_image_dims=None):
     image_pil = Image.fromarray(image_numpy)
     h, w, _ = image_numpy.shape
 
-    if aspect_ratio > 1.0:
-        image_pil = image_pil.resize((h, int(w * aspect_ratio)), Image.BICUBIC)
-    if aspect_ratio < 1.0:
-        image_pil = image_pil.resize((int(h / aspect_ratio), w), Image.BICUBIC)
-    image_pil.save(image_path)
+    if original_image_dims is not None:
+        image_pil = image_pil.resize(original_image_dims, Image.ANTIALIAS)
+    image_pil.save(image_path, quality=95)
 
 
 def mkdirs(paths):
