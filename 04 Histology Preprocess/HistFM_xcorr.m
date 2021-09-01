57d3784dc073bf2d9f29dac135dd871c685f2b3f
Jessica <jt658@stanford.edu>
2021-09-01 10:22:46 -0700

    Simplified the histology to fluoresence image registration to use MATLAB in-built image registration. Also changed default folder for svs files to Aidan's folder

--- 04 Histology Preprocess/HistFM_xcorr.m
+++ 04 Histology Preprocess/HistFM_xcorr.m
@@ -1,9 +1,13 @@
 function [tform, isHistImageFlipped] = HistFM_xcorr(imFM, imPB, imHist)
-%This uses normalized cross correlation to align the input
-%the histology slices with input flourecence microscopy images
+%This uses the input MATLAB one-plus-one evolutionary optimizer 
+%configuration and rigid transformations (rotation and translation) to 
+%align the input histology slices with input flourecence microscopy images
 %
-%It finds the correct rotation, translation, and horiztonal flip which
-%maximizes the normalized cross correlation.
+%It finds the correct rotation and translation which maximizes the Mattes 
+%mutual information metric. It chooses the horizontal flip based on the 
+%maximum of the normalized cross correlation between the registered
+%(flipped and non-flipped) histology slice and the flourecence microscopy 
+%images.
 % 
 %It assumes that there is a known scaling factor between the images (0.7 
 %below), and then performs coarse registration (translation accuracy: 
@@ -18,13 +22,12 @@ function [tform, isHistImageFlipped] = HistFM_xcorr(imFM, imPB, imHist)
 % - tform
 % - isHistImageFlipped
 
+%% Image Pre-Processing Steps
 pxielSize_FM = 0.720915041080197;
 pixelSize_Histology = 0.503661377589388;
 SP52Hist_scale = pixelSize_Histology / pxielSize_FM;
 he_background = 20;
 
-%% Section 0 - read imgs
-% read he images
 he0 = imHist;
 
 % set black artifact in HE scan to background level
@@ -78,138 +81,55 @@ bf = double(bf0)*255/background;
 bf(bf>255) = 255;
 bf = 255- bf;
 
-% place bf image in 0 padded image of same size as HE image
-xind = round(size(he_new,2)/2) - round(size(bf,2)/2) + [1:size(bf,2)];
-yind = round(size(he_new,1)/2) - round(size(bf,1)/2)+ [1:size(bf,1)];
-xind(xind<0) = []; % remove bad indexies
-yind(yind<0) = []; % remove bad indexies
+%% Downscaled Registration
+[optimizer,metric] = imregconfig('multimodal');
 
-bf_new = zeros(size(he_new));
-bf_new0 = zeros(size(he_new));
-try % Add a try catch because we are getting an error on this line but not sure why, after we get an answer we can remove catch clause
-    bf_new(yind,xind) = bf;
-catch e
-    disp('Here are varibles that might help troubleshoot:');
-    yind
-    xind
-    rethrow(e);
-end
-bf_new0(yind,xind) = bf0;
-%% Section 1 - coarse registration
+% Apply histogram equalization. This step helps improve registration
+% accuracy
+he_new_hist_eq = histeq(he_new);
+he_new_fliplr1_hist_eq = histeq(he_new_fliplr);
+bf1_hist_eq = histeq(bf);
 
-% scale down images for coarse registration
-downscale = 1/16;
+% Scale down the images 
+downscale = 1/8;
+he_new_downscaled = imresize(he_new_hist_eq, downscale);
+he_new_fliplr_downscaled = imresize(he_new_fliplr1_hist_eq, downscale);
+bf_downscaled = imresize(bf1_hist_eq, downscale);
 
-he_new2 = imresize(he_new,downscale);
-he_new2_fliplr = imresize(he_new_fliplr,downscale);
-bf_new2 = imresize(double(bf_new),downscale);
+tform_fliplr = imregtform(he_new_fliplr_downscaled,bf_downscaled,'rigid',optimizer,metric);
+tform_normal = imregtform(he_new_downscaled,bf_downscaled,'rigid',optimizer,metric);
 
-% normalized cross-correlation over all angles and flipped and not flipped
-% orientations (takes 5 minutes)
-angles = [0:1:360];
-for rot_i = 1:length(angles)
-    
-    a = he_new2;
-    b = imrotate(bf_new2,angles(rot_i),'crop');
-    
-    C(:,:,rot_i) = normxcorr2(a,b);
-end
+% Fix Translation Factor Due to Downscaling
+tform_fliplr.T(3,1) = tform_fliplr.T(3,1) * 8;
+tform_fliplr.T(3,2) = tform_fliplr.T(3,2) * 8;
+
+tform_normal.T(3,1) = tform_normal.T(3,1) * 8;
+tform_normal.T(3,2) = tform_normal.T(3,2) * 8;
+
+% Debug Plotting
+%Rfixed = imref2d(size(bf));
+%he_new_fliplr_registered = imwarp(he_new_fliplr,tform_fliplr, 'OutputView', Rfixed);
+%he_new_registered = imwarp(he_new,tform_normal, 'OutputView', Rfixed);
+%figure();imshowpair(he_new_fliplr_registered, bf);title("Flipped Registration");
+%figure();imshowpair(he_new_registered, bf);title("Un-Flipped Registration");
+
+%% Apply registration to histology image
+Rfixed_downscaled = imref2d(size(bf_downscaled));
+he_new_fliplr_reg_downscaled = imwarp(imresize(he_new_fliplr, downscale),tform_fliplr,'OutputView',Rfixed_downscaled);
+he_new_reg_downscaled = imwarp(imresize(he_new, downscale),tform_normal,'OutputView',Rfixed_downscaled);
+
+%% Determine flip orientation 
+flipC = normxcorr2(he_new_fliplr_reg_downscaled, bf_downscaled);
+C = normxcorr2(he_new_reg_downscaled, bf_downscaled);
+
 [xcorr_val,ind]=max(C,[],'all','linear');
+[xcorr_val_fliplr,ind_fliplr]=max(flipC,[],'all','linear');
 
-for rot_i = 1:length(angles)
-    
-    a = he_new2_fliplr;
-    b = imrotate(bf_new2,angles(rot_i),'crop');
-    
-    C_fliplr(:,:,rot_i) = normxcorr2(a,b);
-end
-[xcorr_val_fliplr,ind_fliplr]=max(C_fliplr,[],'all','linear');
-
-isHistImageFlipped = xcorr_val_fliplr > xcorr_val;
-
-% calculate coarse registration offsets and angle
-if isHistImageFlipped
-    he_fine = he_new2_fliplr;
-    [peaky,peakx,peakr] = ind2sub(size(C_fliplr),ind_fliplr);
+if xcorr_val_fliplr > xcorr_val
+    tform = tform_fliplr;
+    isHistImageFlipped = true;
 else
-    he_fine = he_new2;
-    [peaky,peakx,peakr] = ind2sub(size(C),ind);
+    tform = tform_normal;
+    isHistImageFlipped = false;
 end
 
-offsety_c = peaky - size(he_new2,1);
-offsetx_c = peakx - size(he_new2,2);
-rot_angle_c = angles(peakr);
-
-% for debugging
-%figure; imagesc(imfuse(imtranslate(imrotate(bf_new2,rot_angle_c ,'crop'),[-offsetx_c,-offsety_c]),he_fine));
-%% fine registration
-upsample = 1;
-
-% created cropped images for fine registration
-crop_bf = bf;
-% only  approximate translation because he has a slightly different center from bf
-if isHistImageFlipped
-    crop_he = imtranslate(imrotate(fliplr(he),-rot_angle_c ,'crop'),[offsetx_c*1/downscale,offsety_c*1/downscale]);
-else
-    crop_he = imtranslate(imrotate(he,-rot_angle_c ,'crop'),[offsetx_c*1/downscale,offsety_c*1/downscale]);
-end
-
-crop_he = crop_he(yind,xind);
-he_pad = 50;
-crop_he = padarray(crop_he,[he_pad,he_pad]);
-
-crop_bf = imresize(crop_bf,upsample);
-crop_he = imresize(crop_he,upsample);
-
-% perform fine registration
-angles_fine = [-0.5:0.1:0.5];
-
-clearvars F
-for rot_i = 1:length(angles_fine)
-    a=crop_bf;
-    b=imrotate(crop_he,angles_fine(rot_i),'crop');
-    F(:,:,rot_i) = normxcorr2(a,b);
-end
-
-% calculate resulting offsets and angle
-[~,ind]=max(F,[],'all','linear');
-[peaky,peakx,peakr] = ind2sub(size(F),ind);
-offsety = peaky - size(crop_bf,1);
-offsetx = peakx - size(crop_bf,2);
-rot_angle = angles_fine(peakr);
-
-% calculate the final HE image corresponding to input BF
-if isHistImageFlipped
-    he_final =  imtranslate(imrotate(fliplr(he0),-rot_angle_c,'crop'),[offsetx_c*1/downscale,offsety_c*1/downscale]);
-else
-    he_final =  imtranslate(imrotate(he0,-rot_angle_c,'crop'),[offsetx_c*1/downscale,offsety_c*1/downscale]);
-end
-he_final = he_final(yind(1)-he_pad:yind(end)+he_pad,xind(1)-he_pad:xind(end)+he_pad,:);
-he_final = imtranslate(imrotate(he_final,rot_angle,'crop'),[-offsetx*1/upsample,-offsety*1/upsample]);
-he_final = he_final(1:size(bf0,1),1:size(bf0,2),:);
-
-%% find corrresponding tform
-if isHistImageFlipped
-    original = uint8(mean(fliplr(imHist),3));
-else
-    original = uint8(mean(imHist,3));
-end
-distorted = uint8(mean(he_final,3));
-
-% detect and extact features
-ptsOriginal  = detectSURFFeatures(original);
-ptsDistorted = detectSURFFeatures(distorted);
-[featuresOriginal,validPtsOriginal] = ...
-    extractFeatures(original,ptsOriginal);
-[featuresDistorted,validPtsDistorted] = ...
-    extractFeatures(distorted,ptsDistorted);
-
-% match points
-index_pairs = matchFeatures(featuresOriginal,featuresDistorted);
-matchedPtsOriginal  = validPtsOriginal(index_pairs(:,1));
-matchedPtsDistorted = validPtsDistorted(index_pairs(:,2));
-
-% calculate tform
-[tform,~,~] = ...
-    estimateGeometricTransform(matchedPtsOriginal,matchedPtsDistorted,...
-    'similarity');
