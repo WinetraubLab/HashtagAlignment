e120415708d30e4e9685ac180db477720835edbc
MyYo <MyYo@users.noreply.github.com>
2023-03-13 14:05:57 -0700

    add more time to photobleach

--- 00 Calibration and Analysis Scripts/Photobleach Time Calibration/scanTarget_PhotobleachTimes.m
+++ 00 Calibration and Analysis Scripts/Photobleach Time Calibration/scanTarget_PhotobleachTimes.m
@@ -4,8 +4,8 @@
 %% Inputs
 
 % Exposure and n passes settings
-exposures = [1 2 5 10 15 30 45 60]; % Units are sec per 1mm of line
-nPasses =   [2 2 2 2   2  3  3  3]; % Number of passes should be as low as possible but still allow OCT scanner not to crash
+exposures = [30 45 60 90 120 180]; % Units are sec per 1mm of line
+nPasses =   [3  3  3  3   3   4]; % Number of passes should be as low as possible but still allow OCT scanner not to crash
 % For 10x, we usually use: 15 sec/1mm and 2 passes.
 % For 40x, we usually use: 5  sec/1mm and 2 passes.
 
@@ -16,6 +16,9 @@ lineLength = 0.5; %mm
 % z = 0 is the tissue/air interface.
 zDepths = [-0.300, 0.000, 0.050, 0.100]; %Photobleach line depth. mm. +z means deeper
 
+% Set to true if you want to do a dry run
+skipHardware = false;
+
 %% Photobleach loop
 for i=1:length(exposures)
     fprintf('%s Photobleaching line #%d. Exposure: %.1f sec/mm, nPasses: %d.\n', ...
@@ -25,13 +28,15 @@ for i=1:length(exposures)
     yOCTPhotobleachTile([0;-lineLength/2],[0;+lineLength/2], ...
         'octProbePath',octProbePath, ...
         'exposure',exposures(i),'nPasses',nPasses(i),...
-        'z',zDepths);
+        'z',zDepths,'skipHardware',skipHardware,'plotPattern',true);
     
     pause(0.5);
     
     %Translate stage by a little
-    [x0,y0,z0] = yOCTStageInit();
-    yOCTStageMoveTo(x0+lineLength/4,y0,z0);
+    if ~skipHardware
+        [x0,y0,z0] = yOCTStageInit();
+        yOCTStageMoveTo(x0+lineLength/4,y0,z0);
+    end
 
     pause(0.5);
 end
