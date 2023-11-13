3c78fd6f7698b861b6a2c390f1bc8b74cb8689ce
MyYo <MyYo@users.noreply.github.com>
2023-11-12 20:00:01 -0800

    Add capability to download two types of datasets

--- 23 Visualize Results/scriptViewDataset.m
+++ 23 Visualize Results/scriptViewDataset.m
@@ -3,10 +3,15 @@
 
 %% Inputs
 imageResolution = '10x';
-datasetTag = '';%'2020-08-15'; % What date this data set was created, leave empty for latest
+datasetTag = '';%'2020-11-10'; % What date this data set was created, leave empty for latest
 outputDir = 'tmpDataset';
+
+isRaw = false; % Set to ture to view the raw images, false to download the view for user
 
 %% Gather dataset
 datasetPath = s3GetPathToLatestDataset(imageResolution,datasetTag);
 awsMkDir([pwd '/' outputDir '/'],true);
-awsCopyFileFolder([datasetPath 'original_image_pairs_view_for_user/'],[outputDir '/']);
+if isRaw
+    awsCopyFileFolder([datasetPath 'original_image_pairs/'],[outputDir '/']);
+else
+    awsCopyFileFolder([datasetPath 'original_image_pairs_view_for_user/'],[outputDir '/']);
